2b77d62ef1f4d0a44e0988dfb10738b82ffe9c91
= <=>
2023-12-03 14:17:29 +0300

    Проверка калькулятора

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ import util
 a = 21
 b = 44
 print(util.num_input(a, b))
+
+ans1 = util.num_input(1, 4)
+
+if ans1 == 1:
+    print(calc.sum(a, b))
+
+elif ans1 == 2:
+    print(calc.sub(a, b))
+
+elif ans1 == 3:
+    print(calc.mult(a,b))
+
+elif ans1 == 4:
+    print(calc.div(a,b))
